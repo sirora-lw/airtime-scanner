af41e1e13d32d06df37b8953b3098010616893c0
Leslie <91218588+sirora-lw@users.noreply.github.com>
2025-07-05 00:52:08 +0200

    Add files via upload

---
+++ airtime-scanner/src/polyfills.ts
@@ -0,0 +1,75 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This file is divided into 2 sections:
+ * 1. Browser polyfills. These are applied before loading Zone.js and are sorted by browsers.
+ * 2. Application imports. Files imported after Zone.js that should be loaded before your main file.
+ *
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes Safari, Chrome, Firefox, Opera, Edge on desktop,
+ * and iOS, Android, and Windows Phone.
+ *
+ * Learn more in https://angular.io/guide/browser-support
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+import 'zone.js';  // Included with Angular CLI.
+
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+// Add any other application-specific polyfills here if needed.
+// import 'core-js/es/array';
+// import 'core-js/es/date';
+// import 'core-js/es/function';
+// import 'core-js/es/map';
+// import 'core-js/es/math';
+// import 'core-js/es/number';
+// import 'core-js/es/object';
+// import 'core-js/es/parse-float';
+// import 'core-js/es/parse-int';
+// import 'core-js/es/regexp';
+// import 'core-js/es/string';
+// import 'core-js/es/symbol';
+// import 'core-js/es/set';
+// import 'core-js/es/promise';
+// import 'core-js/es/weak-map';
+// import 'core-js/es/weak-set';
+// import 'core-js/es/array/from';
+// import 'core-js/es/array/iterator';
+// import 'core-js/es/array/of';
+// import 'core-js/es/array/reduce';
+// import 'core-js/es/array/splice';
+// import 'core-js/es/date/now';
+// import 'core-js/es/map/from';
+// import 'core-js/es/map/iterator';
+// import 'core-js/es/map/of';
+// import 'core-js/es/number/is-finite';
+// import 'core-js/es/number/is-nan';
+// import 'core-js/es/object/assign';
+// import 'core-js/es/object/set-prototype-of';
+// import 'core-js/es/object/values';
+// import 'core-js/es/promise/finally';
+// import 'core-js/es/string/includes';
+// import 'core-js/es/string/starts-with';
+// import 'core-js/es/string/ends-with';
+// import 'core-js/es/string/pad-start';
+// import 'core-js/es/string/pad-end';
+// import 'core-js/es/string/trim-start';
+// import 'core-js/es/string/trim-end';
+// import 'core-js/es/symbol/async-iterator';
+// import 'core-js/es/symbol/iterator';
+// import 'core-js/es/symbol/to-primitive';
+// import 'core-js/es/symbol/unscopables';
+// import 'core-js/es/weak-map/from';
+// import 'core-js/es/weak-map/iterator';
+// import 'core-js/es/weak-map/of';
+// import 'core-js/es/weak-set/from';
+// import 'core-js/es/weak-set/iterator';
+// import 'core-js/es/weak-set/of';
